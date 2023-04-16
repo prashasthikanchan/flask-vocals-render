27ada12bc77ef969bb9893846437bfebd0decaa7
Prashasthi K Kanchan <75254746+prashasthikanchan@users.noreply.github.com>
2023-04-16 11:52:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+import pyttsx3
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+
+from firebase import firebase
+
+from flask import Flask, jsonify, request
+firebase = firebase.FirebaseApplication('https://vocals-e4589-default-rtdb.asia-southeast1.firebasedatabase.app/', None)
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return 'Book your appointments'
